3c945e80caee4d6098df7552eb1b0551c7935253
Aashutosh Basnet <aashutoshbasnet3344@gmail.com>
2025-02-09 09:25:16 +0530

    "cors"

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 const config = {
   PORT: process.env.PORT || 3001,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CORS_ORIGINS: process.env.CORS_ORIGINS ? JSON.parse(process.env.CORS_ORIGINS) : ["http://localhost:5173"],
+  CORS_ORIGINS: process.env.CORS_ORIGINS ? JSON.parse(process.env.CORS_ORIGINS) : ["https://uwrbfh.onrender.com"],
   RATE_LIMIT: {
     windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS) || 15 * 60 * 1000, // 15 minutes
     max: parseInt(process.env.RATE_LIMIT_MAX) || 100,
